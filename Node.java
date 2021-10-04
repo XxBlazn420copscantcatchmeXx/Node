f7f3dbeafd64ba97695c54291479eb587640fe4d
XxBlazn420copscantcatchmeXx <69626422+XxBlazn420copscantcatchmeXx@users.noreply.github.com>
2021-10-04 17:24:55 +0200

    Add files via upload

--- Node.java
+++ Node.java
@@ -1,5 +1,4 @@
-Node.java
-class Node {
+public class Node {
   int data;
   Node next;
 
@@ -9,7 +8,7 @@ class Node {
   }
 }
 
-LinkedList.java
+public class LinkedList.java{
 class LinkedList {
   Node head;
 
@@ -17,21 +16,22 @@ class LinkedList {
     this.head = head;
   }
 
-  void add(Node node){
+   void add(Node node){
     Node current = head;
     while(current.next != null){
       current = current.next;
     }
     current.next = node;
+    }
   }
 }
 
 Main.java
 class Main{
-    public static void main(String[]args){
-    LinkedList linkedList = new LinkedList(new Node(5, new Node(7, new Node(2, new Node(15, null)))));
-    linkedList.add(new Node(10, null));
-    linkedList.head = new Node(50, linkedList.head);
-    linkedList.head.next = new Node(50, linkedList.head.next);
+ public static void main(String[]args){
+  LinkedList linkedList = new LinkedList(new Node(5, new Node(7, new Node(2, new Node(15, null)))));
+  linkedList.add(new Node(10, null));
+  linkedList.head = new Node(50, linkedList.head);
+  linkedList.head.next = new Node(50, linkedList.head.next);
   }
 }
